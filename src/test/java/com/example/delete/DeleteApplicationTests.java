aca31cca7fc93ca9fdd1346b5f9950f1f02f13b1
asgerbirk <asger.birk@gmail.com>
2022-09-13 11:15:04 +0200

    hejwaldt

--- src/test/java/com/example/delete/DeleteApplicationTests.java
+++ src/test/java/com/example/delete/DeleteApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.delete;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/*@SpringBootTest/*
+
+ */
 class DeleteApplicationTests {
 
     @Test
